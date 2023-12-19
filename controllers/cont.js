565ffe4b31044952c9e5ae9b44f8de67c3b6a571
unknown <64825962+D1urga@users.noreply.github.com>
2023-12-19 13:52:29 +0530

    changes in data

--- controllers/cont.js
+++ controllers/cont.js
@@ -1,10 +1,17 @@
 const data = { name: "anoop", edu: "vit" };
+const data2 = {
+  employees: [
+    { name: "Shyam", email: "shyamjaiswal@gmail.com" },
+    { name: "Bob", email: "bob32@gmail.com" },
+    { name: "Jai", email: "jai87@gmail.com" },
+  ],
+};
 
 const cont1 = (req, res) => {
   res.send("hello");
 };
 const cont2 = (req, res) => {
-  res.json(data);
+  res.json(data2);
 };
 
 export { cont1, cont2 };
